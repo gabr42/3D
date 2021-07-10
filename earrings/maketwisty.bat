1bce5f6c81b5c584c3adb8ff7145966b5f8ecff8
gabr <gabr42@gmail.com>
2021-07-10 12:25:16 +0200

    multi-speed vase earrings

---
+++ earrings/maketwisty.bat
@@ -0,0 +1,15 @@
+"c:\Program Files\Prusa3D\PrusaSlicer\prusa-slicer-console.exe" -g -max-print-speed 20 -output twisty1-20.gcode twisty1.3mf
+"c:\Program Files\Prusa3D\PrusaSlicer\prusa-slicer-console.exe" -g -max-print-speed 15 -output twisty1-15.gcode twisty1.3mf
+"c:\Program Files\Prusa3D\PrusaSlicer\prusa-slicer-console.exe" -g -max-print-speed 10 -output twisty1-10.gcode twisty1.3mf
+"c:\Program Files\Prusa3D\PrusaSlicer\prusa-slicer-console.exe" -g -max-print-speed 7.5 -output twisty1-7_5.gcode twisty1.3mf
+"c:\Program Files\Prusa3D\PrusaSlicer\prusa-slicer-console.exe" -g -max-print-speed 5 -output twisty1-5.gcode twisty1.3mf
+
+h:\RAZVOJ\3D\utilities\combiner\Win32\Debug\Combine.exe twisty1-a.gcode twisty1-20.gcode 50 twisty1-15.gcode 60 twisty1-10.gcode 67 twisty1-7_5.gcode 75 twisty1-5.gcode
+
+del twisty1-20.gcode
+del twisty1-15.gcode
+del twisty1-10.gcode
+del twisty1-7_5.gcode
+del twisty1-5.gcode
+
+h:\RAZVOJ\3D\utilities\mirror\Win32\Debug\Mirror.exe twisty1-b.gcode twisty1-a.gcode x125
