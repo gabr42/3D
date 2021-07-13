805aa829dd4e30f44f66f9d714df1f821d3c5d4c
gabr <gabr42@gmail.com>
2021-07-13 13:58:50 +0200

    normal-sized version

--- earrings/maketwisty.bat
+++ earrings/maketwisty.bat
@@ -4,7 +4,7 @@
 "c:\Program Files\Prusa3D\PrusaSlicer\prusa-slicer-console.exe" -g -max-print-speed 7.5 -output twisty1-7_5.gcode twisty1.3mf
 "c:\Program Files\Prusa3D\PrusaSlicer\prusa-slicer-console.exe" -g -max-print-speed 5 -output twisty1-5.gcode twisty1.3mf
 
-h:\RAZVOJ\3D\utilities\combiner\Win32\Debug\Combine.exe twisty1-a.gcode twisty1-20.gcode 50 twisty1-15.gcode 60 twisty1-10.gcode 67 twisty1-7_5.gcode 75 twisty1-5.gcode
+h:\RAZVOJ\3D\utilities\combiner\Win32\Debug\Combine.exe twisty1-a.gcode twisty1-20.gcode 30 twisty1-15.gcode 40 twisty1-10.gcode 45 twisty1-7_5.gcode 60 twisty1-5.gcode
 
 del twisty1-20.gcode
 del twisty1-15.gcode
