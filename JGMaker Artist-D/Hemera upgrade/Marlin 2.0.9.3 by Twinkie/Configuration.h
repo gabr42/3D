01d51cb48bb1ad298f520a05b23e333a43b319ef
gabr <gabr42@gmail.com>
2022-08-28 18:52:48 +0200

    long filenames & skew correction

--- JGMaker Artist-D/Hemera upgrade/Marlin 2.0.9.3 by Twinkie/Configuration.h
+++ JGMaker Artist-D/Hemera upgrade/Marlin 2.0.9.3 by Twinkie/Configuration.h
@@ -1805,17 +1805,17 @@
  *    +-------------->X     +-------------->X     +-------------->Y
  *     XY_SKEW_FACTOR        XZ_SKEW_FACTOR        YZ_SKEW_FACTOR
  */
-//#define SKEW_CORRECTION
+#define SKEW_CORRECTION
 
 #if ENABLED(SKEW_CORRECTION)
   // Input all length measurements here:
-  #define XY_DIAG_AC 282.8427124746
-  #define XY_DIAG_BD 282.8427124746
-  #define XY_SIDE_AD 200
+  #define XY_DIAG_AC 99.46
+  #define XY_DIAG_BD 100.0267
+  #define XY_SIDE_AD 70.5427
 
   // Or, set the default skew factors directly here
   // to override the above measurements:
-  #define XY_SKEW_FACTOR 0.0
+  //#define XY_SKEW_FACTOR 0.0
 
   //#define SKEW_CORRECTION_FOR_Z
   #if ENABLED(SKEW_CORRECTION_FOR_Z)
